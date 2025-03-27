b468988c04ac4143974f3e27d1c4f48854e44408
DevYayaFarias <minvicflg@gmail.com>
2025-03-27 11:21:17 -0300

    Create ex05.py

---
+++ ex05.py
@@ -0,0 +1,11 @@
+preço = {
+    "Norte":(500, 900),
+    "Nordeste":(350, 650),
+    "Centro-Oeste": (350, 600),
+    "Sul": (300, 550)
+}
+
+Print("Destinos disponiveis: 1- Norte, 2-Nordeste, 3-Centro-Oeste ou 4-Sul")
+Destino = input("Informe seu destino: ").lower()
+passagem = input("Passagem só de ida ou de ida e volta?: ")
+ida_volta
