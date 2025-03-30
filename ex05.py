c57b6d87fe37965e057ddd7234013dd7fd8f89a8
DevYayaFarias <minvicflg@gmail.com>
2025-03-30 19:41:54 -0300

    Update ex05.py

--- ex05.py
+++ ex05.py
@@ -1,11 +1,16 @@
-preço = {
-    "Norte":(500, 900),
-    "Nordeste":(350, 650),
+preco = {
+    "Norte": (500, 900),
+    "Nordeste": (350, 650),
     "Centro-Oeste": (350, 600),
     "Sul": (300, 550)
 }
 
-Print("Destinos disponiveis: 1- Norte, 2-Nordeste, 3-Centro-Oeste ou 4-Sul")
-Destino = input("Informe seu destino: ").lower()
-passagem = input("Passagem só de ida ou de ida e volta?: ")
-ida_volta
+print("Destinos disponíveis: 1- Norte, 2- Nordeste, 3- Centro-Oeste ou 4- Sul")
+destino = input("Informe seu destino: ").strip().title()
+
+if destino in preco:
+    ida_volta = input("Ida e volta? (s/n): ").strip().lower() == "s"
+    valor = preco[destino][1 if ida_volta else 0]
+    print(f"Preço: R$ {valor:.2f}")
+else:
+    print("Destino inválido!")
