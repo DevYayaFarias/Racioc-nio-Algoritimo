b3de48a836a5e06daab8a3b9cb086c95398e3afc
DevYayaFarias <minvicflg@gmail.com>
2025-04-07 23:32:06 -0300

    Create TO-EX08.py

---
+++ Exercícios: laços/TO-EX08.py
@@ -0,0 +1,9 @@
+binario = input("Digite um número binário: ")
+decimal = 0
+expoente = len(binario) - 1
+
+for digito in binario:
+    decimal += int(digito) * (2 ** expoente)
+    expoente -= 1
+
+print(f"Decimal: {decimal}")
