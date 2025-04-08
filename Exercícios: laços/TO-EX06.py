f50895da8eec6ca100d18f501bf0b0c9c37df71a
DevYayaFarias <minvicflg@gmail.com>
2025-04-08 09:08:13 -0300

    Update TO-EX06.py

--- Exercícios: laços/TO-EX06.py
+++ Exercícios: laços/TO-EX06.py
@@ -1,2 +1,11 @@
 nome = input("Digite seu nome completo: ").split()
 print(f"Primeiro nome: {nome[0]}, Último nome: {nome[-1]}")
+
+#Outra opção vista em sala de aula:
+name = input("Digite seu nome completo: ")
+first = ""
+for l in name:
+  if l == " ":
+    break
+  first +=l
+print(first)
