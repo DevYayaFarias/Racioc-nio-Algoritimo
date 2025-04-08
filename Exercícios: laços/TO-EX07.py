1ac8b41cbaa77b631b5ce55203da5ec715bbabcd
DevYayaFarias <minvicflg@gmail.com>
2025-04-08 09:03:00 -0300

    Update TO-EX07.py

--- Exercícios: laços/TO-EX07.py
+++ Exercícios: laços/TO-EX07.py
@@ -6,3 +6,16 @@ while True:
     cont += palavra.count('a')
 
 print(f"Total de letras 'A': {cont}")
+
+
+#OUTRA OPÇÃO, VISTO EM SALA:
+cont = 0
+while True:
+    palavra = input("Digite uma palavra (<enter> para parar): ").lower()
+    if palavra == "":
+        break
+    for l in palavra:
+        if l == "a" or l =="A":
+        cont += 1
+
+print(f"Total de letras 'A': {cont}")
